252961ee61af4e3c3d61b95ee850d16ce4141c65
paul pham <paulpham3000@gmail.com>
2023-04-19 18:36:37 +1000

    updated vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/flower-books',
   plugins: [react()],
   test: {
     globals: true, // so we don't need to import describe and it every time
